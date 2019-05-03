7721c5bfe0a19836b0577b8e7b1d1fedf181161e
Joshua Suskin <jsuskin85@gmail.com>
2019-05-03 07:40:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 def speak_to_grandma(str)
   if str == str.upcase
+    if str == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    else
+      return "NO, NOT SINCE 1938!"
+    end
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+    
 end
 
 # Whatever you say to grandma, she should respond with
